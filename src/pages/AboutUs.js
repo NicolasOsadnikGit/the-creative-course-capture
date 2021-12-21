97cfa83b71dc40a301625b9b60bb9e66361d8b25
Nicolas <nicolas.osadnik@osapiens.com>
2021-12-21 21:56:05 +0100

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <Fragment>
+      <AboutSection />
+      <ServicesSection />
+    </Fragment>
+  );
 };
 
 export default AboutUs;
